f20adb50b569289a9f0733f492e9fbfabf2c0ac0
gauravp31 <gaurav21@kretacx.com>
2023-08-09 13:48:26 +0530

    Deployment message add

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 app.get('/', (req, res) => {
-    res.send('Deployment successful!')
+    res.send('Deployment successful!!!!')
 })
 
 
